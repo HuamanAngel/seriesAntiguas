d7c7d3af26755deb4ee930d8d766a6f6779896a2
HuamanAngel <oxipusioxx12@gmail.com>
2020-12-31 15:51:38 -0500

    Proyecto base

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+    // $host = "mysql:localhost:3306";
+    // $username = "root";
+    // $pass = "oxipusio";
+    // $dbname = "prueba2";
+
+    // $mysqli = new mysqli($host,$username,$pass,$dbname);
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+    require_once "controller/HomeController.php";
+    $home = new HomeController();
+    $home->index();
+?>
